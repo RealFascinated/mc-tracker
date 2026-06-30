70ab11c74bcd8f1a860740c03c967977034b4f71
fascinated <liam@fascinated.cc>
2026-06-30 23:44:42 +0100

    fix

--- www/src/lib/minecraft-default-favicon.ts
+++ www/src/lib/minecraft-default-favicon.ts
@@ -1,2 +1,2 @@
 /** Vanilla Java Edition server list icon (`unknown_server.png`). */
-export const MINECRAFT_DEFAULT_SERVER_FAVICON = "/minecraft-unknown-server.png";
+export const MINECRAFT_DEFAULT_SERVER_FAVICON = `${import.meta.env.BASE_URL}minecraft-unknown-server.png`;
