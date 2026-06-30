5155f9f3cefa095ae22d0a56ea324292a162c54a
fascinated <liam@fascinated.cc>
2026-06-30 18:38:22 +0100

    remove file logging

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,5 +1,4 @@
 import Winston, { format } from "winston";
-import DailyRotateFile from "winston-daily-rotate-file";
 const { colorize, timestamp, printf } = format;
 
 const customFormat = format.combine(
@@ -16,14 +15,6 @@ export const logger = Winston.createLogger({
   transports: [
     new Winston.transports.Console({
       format: Winston.format.combine(colorize(), customFormat),
-    }),
-    new DailyRotateFile({
-      filename: "data/logs/%DATE%.log",
-      datePattern: "YYYY-MM-DD",
-      format: Winston.format.combine(customFormat),
-      maxFiles: 3, // Keep last 3 files
-      maxSize: "5m", // 5MB
-      zippedArchive: true,
     }),
   ],
 });
